bdd455b61be36798ae4b8b9b10393bec3de51bd7
cristim806 <cristian.merticaru8806@yahoo.com>
2018-01-07 17:05:06 +0200

    Generation of Zeckendorf numbers.

---
+++ ZeckendorfNumbers/src/zeckendorf/Zeckendorf.java
@@ -0,0 +1,43 @@
+package zeckendorf;
+import java.math.BigInteger;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Zeckendorf {
+	
+	
+	public static Map<Integer, BigInteger> retrieveZeckendorfNumbers(BigInteger from, BigInteger to) throws Exception{
+		if ((from.intValue() >= 0) && (to.intValue() <=  2147483647) && (from.intValue() < to.intValue())){
+				
+			List<Integer> fibSeqNumbers = ZeckendorfUtils.retrieveFibonacciSeqNumbers(to.intValue());
+			Collections.reverse(fibSeqNumbers);
+			Map<Integer, BigInteger> zeckendorfNumbers = new HashMap<>();
+			
+			for (int i = from.intValue() + 1; i < to.intValue(); i++){
+				zeckendorfNumbers.put(i, ZeckendorfUtils.calculateZeckendorfNumber(i, fibSeqNumbers));
+			}
+			return zeckendorfNumbers;
+		}else throw new Exception(" The interval numbers must be between 0 and 2,147,483,647 ");
+		
+	}
+
+	public static void main(String... args) {
+
+		Map<Integer, BigInteger> zeckendorfNumbers;
+		try {
+			zeckendorfNumbers = retrieveZeckendorfNumbers(new BigInteger("4"), new BigInteger("154"));
+			
+			zeckendorfNumbers.forEach((k,v)->{
+				System.out.println("Number : " + k + " has Zeckendorf number : " + v);
+				
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		
+	}
+
+}
---
+++ ZeckendorfNumbers/src/zeckendorf/ZeckendorfUtils.java
@@ -0,0 +1,50 @@
+package zeckendorf;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ZeckendorfUtils {
+	
+	public static final BigInteger BigInteger_10 = BigInteger.TEN;
+	
+	
+	public static List<Integer> retrieveFibonacciSeqNumbers(int number){
+		List<Integer> fibonacciSeqList = new ArrayList<>();
+		int fibonacci=1;
+		
+		if(number == 1 || number == 2){
+			fibonacciSeqList.add(fibonacci);
+        }else{
+        	fibonacciSeqList.add(fibonacci);
+        	int firstNumber=1;
+        	int nextNumber=1;
+        	while ((fibonacci = firstNumber + nextNumber) <= number){					           
+        		firstNumber = nextNumber;
+        		nextNumber = fibonacci;
+                fibonacciSeqList.add(fibonacci);
+              
+    		}
+        }
+        return fibonacciSeqList;
+    }
+	
+	public static BigInteger calculateZeckendorfNumber(int number,
+			List<Integer> fibonacciNumbers) {
+	
+		int numbersLength = fibonacciNumbers.size() - 1;
+		for (Integer value : fibonacciNumbers) {
+			if (number >= value) {
+				BigInteger bigNr = (numbersLength == 0) ?  BigInteger.ONE : BigInteger_10.pow(numbersLength);
+						
+				numbersLength--;
+				return bigNr.add(calculateZeckendorfNumber(number - value,
+						fibonacciNumbers));
+			}else{
+				numbersLength--;
+			}
+		}
+		return BigInteger.ZERO;
+	}
+
+}
